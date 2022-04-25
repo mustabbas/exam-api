e7688cd9fa64b97a7daa6d6e22271a978a3a5fc4
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-04-25 23:34:25 +0300

    update cors url

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module ExamApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://mustexamfront.herokuapp.com/'
+        origins 'https://mustexamfront.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
       end
     end
